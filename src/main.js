80e755326516115729817afcc2dce91d7bb1ee14
Florian Saathoff <florian.saathoff@gmx.de>
2022-09-28 13:44:34 +0200

    # Make it use a cookie

--- src/main.js
+++ src/main.js
@@ -1,85 +1,39 @@
+let listOfItemsForSelectBox = [
+    'Baden-Württemberg', 
+    'Bayern', 
+    'Berlin', 
+    'Brandenburg', 
+    'Bremen', 
+    'Hamburg', 
+    'Hessen', 
+    'Mecklenburg-Vorpommern', 
+    'Niedersachsen', 
+    'Nordrhein-Westfalen', 
+    'Rheinland-Pfalz', 
+    'Saarland', 
+    'Sachsen', 
+    'Sachsen-Anhalt', 
+    'Schleswig-Holstein', 
+    'Thüringen']
+
 window.onload = (event) => {
     console.log('page is fully loaded');
-    if(document.cookie == undefined){
-
+    if(sessionStorage.getItem("Bundesland") == null){
         Swal.fire({
-        title: '<strong>Huch, du bist scheinbar noch nich angemeldet</strong>',
-        icon: 'info',
-        html:
-        '      <form>'+
-        '<div class="form-group">'+
-        '  <label>Bundesland: </label> <select class="form-control" name="Bundeslaender">'+
-        '    <option value="BW">'+
-        '      Baden-Württemberg'+
-        '    </option>'+
-
-        '    <option value="BY">'+
-        '      Bayern'+
-        '    </option>'+
-
-        '    <option value="BE">'+
-        '      Berlin'+
-        '    </option>'+
-
-        '    <option value="BB">'+
-        '      Brandenburg'+
-        '    </option>'+
-
-        '    <option value="HB">'+
-        '      Bremen'+
-        '    </option>'+
-
-        '    <option value="HH">'+
-        '      Hamburg'+
-        '    </option>'+
-
-        '    <option value="HE">'+
-        '      Hessen'+
-        '    </option>'+
-
-        '    <option value="MV">'+
-        '      Mecklenburg-Vorpommern'+
-        '    </option>'+
-
-        '    <option value="NI">'+
-        '      Niedersachsen'+
-        '    </option>'+
-
-        '    <option value="NW">'+
-        '      Nordrhein-Westfalen'+
-        '    </option>'+
-
-        '    <option value="RP">'+
-        '      Rheinland-Pfalz'+
-        '    </option>'+
-
-        '    <option value="SL">'+
-        '      Saarland'+
-        '    </option>'+
-
-        '    <option value="SN">'+
-        '      Sachsen'+
-        '    </option>'+
-
-        '    <option value="ST">'+
-        '      Sachsen-Anhalt'+
-        '    </option>'+
-
-        '    <option value="SH">'+
-        '      Schleswig-Holstein'+
-        '    </option>'+
-
-        '    <option value="TH">'+
-        '      Thüringen'+
-        '    </option>'+
-        '  </select>'+
-        '</div>'+
-        ' </form>',
-        showConfirmButton: true,
-        showCancelButton: true,
-        confirmButtonText: 'Anmelden',
-        cancelButtonText: 'Abbrechen',
-        focusConfirm: false
-        }) 
+            title: 'Huch, du hast scheinbar noch kein Bundesland hinterlegt',
+            input: 'select',
+            inputOptions: listOfItemsForSelectBox,
+            inputPlaceholder: 'Wähle dein Bundesland',
+            showCancelButton: true,
+            icon: 'info',
+            inputValidator: function (value) {
+                return new Promise(function (resolve, reject) {
+                    if (value != null) {
+                        resolve()
+                    }
+                })
+            }}).then(function (result) {
+                sessionStorage.setItem("Bundesland", result);
+        })
     }
 }
