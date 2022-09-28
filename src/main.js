07a4b2745289e67b5db9d392dbd749015e1a19f1
Tim Dittmer <schule@freizeit-meer.de>
2022-09-28 13:07:35 +0200

    Bundesländer in Popup hinzugefügt

--- src/main.js
+++ src/main.js
@@ -1,25 +1,78 @@
-/* window.onload = (event) => {
+window.onload = (event) => {
     console.log('page is fully loaded');
     Swal.fire({
-        title: '<strong>Huch, du bist scheinbar noch nich angemeldet</strong>',
+        title: '<strong>Huch, du bist scheinbar noch nicht angemeldet <img src="ico/sad_smile.png" id="sadsmile">  </strong>',
         icon: 'info',
         html:
-        '<form>'+
-        '    <div class="form-group">'+
-        '        <label>Beschriftung Selectbox</label> '+
-        '        <select class="form-control" name="selectbox_name">'+
-        '            <option value="Wert 1">'+
-        '                Antwort 1'+
-        '            </option>'+
-        '            <option value="Wert 2">'+
-        '                Antwort 2'+
-        '            </option>'+
-        '        </select>'+
-        '    </div>'+
-        '    <div class="form-group">'+
-        '        <label>Beschriftung Textfeld</label> <input type="text" class="form-control" name="text_name" placeholder="Placeholder Textfeld">'+
-        '    </div>'+
-        '</form>',
+ '      <form>'+
+ '<div class="form-group">'+
+ '  <label>Bundesland: </label> <select class="form-control" name="Bundeslaender">'+
+ '    <option value="BW">'+
+ '      Baden-Württemberg'+
+ '    </option>'+
+
+ '    <option value="BY">'+
+ '      Bayern'+
+ '    </option>'+
+
+ '    <option value="BE">'+
+ '      Berlin'+
+ '    </option>'+
+
+ '    <option value="BB">'+
+ '      Brandenburg'+
+ '    </option>'+
+
+ '    <option value="HB">'+
+ '      Bremen'+
+ '    </option>'+
+
+ '    <option value="HH">'+
+ '      Hamburg'+
+ '    </option>'+
+
+ '    <option value="HE">'+
+ '      Hessen'+
+ '    </option>'+
+
+ '    <option value="MV">'+
+ '      Mecklenburg-Vorpommern'+
+ '    </option>'+
+
+ '    <option value="NI">'+
+ '      Niedersachsen'+
+ '    </option>'+
+
+ '    <option value="NW">'+
+ '      Nordrhein-Westfalen'+
+ '    </option>'+
+
+ '    <option value="RP">'+
+ '      Rheinland-Pfalz'+
+ '    </option>'+
+
+ '    <option value="SL">'+
+ '      Saarland'+
+ '    </option>'+
+
+ '    <option value="SN">'+
+ '      Sachsen'+
+ '    </option>'+
+
+ '    <option value="ST">'+
+ '      Sachsen-Anhalt'+
+ '    </option>'+
+
+ '    <option value="SH">'+
+ '      Schleswig-Holstein'+
+ '    </option>'+
+
+ '    <option value="TH">'+
+ '      Thüringen'+
+ '    </option>'+
+ '  </select>'+
+ '</div>'+
+' </form>',
         showConfirmButton: true,
         showCancelButton: true,
         confirmButtonText: 'Anmelden',
@@ -27,4 +80,3 @@
         focusConfirm: false
     })
 }
- */
